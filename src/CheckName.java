57085d9ba88e56fbf2b2668bdec0441ec39072cc
skirflex <elbrus98>
2021-03-02 23:02:35 +0300

    new exp

--- src/CheckName.java
+++ src/CheckName.java
@@ -6,24 +6,40 @@ public class CheckName {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int spaceIndex = 32;
+        String [] array;
         System.out.println("Введите ФИО");
         String text = scanner.nextLine().trim();
+
+        //================================================ Первый способ
+        int spaceIndex = 32;
         if (text.indexOf(spaceIndex) == -1) {
             System.out.println("Неправильно написано ФИО");
-        } else {
+        } else
+            {
             String surname = text.substring(0, text.indexOf(spaceIndex)).trim();
             String patronymic = text.substring(text.lastIndexOf(spaceIndex)).trim();
             String name = text.substring(text.indexOf(32), text.lastIndexOf(spaceIndex)).trim();
             if (name.indexOf(32) <= 0 && surname.indexOf(32) <= 0 && patronymic.indexOf(32) <= 0 && name.length() != 0 && patronymic.length() != 0) {
-                System.out.println("Фамилия :" + surname);
+                System.out.println("Фамилия: " + surname);
                 System.out.println("Имя: " + name);
                 System.out.println("Отчество: " + patronymic);
             } else {
                 System.out.println("Неправильно написано ФИО");
             }
-            // Проверка работы
-        }
 
+
+        //================================================ Второй способ
+        }
+        array = text.split("\\b\\s+");
+        if (array.length == 3) {
+            System.out.println("Фамилия: " + array[0]);
+            System.out.println("Имя: " + array[1]);
+            System.out.println("Отчество: " + array[2]);
+
+        }
+        else
+        {
+            System.out.println("Неправильно написано ФИО");
+        }
     }
 }
